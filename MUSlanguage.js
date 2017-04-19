6b56d06fcbb7103fc00f44b1a5f3a6e802d71af3
Andrei B <andrei4.bulzan@gmail.com>
2017-04-19 22:57:04 +0300

    try again

--- MUSlanguage.js
+++ MUSlanguage.js
@@ -22,23 +22,65 @@ var endTime = function (time, expr) {
     return time + duration(expr);
 };
 
-var compile = function (musexpr) {
-    var list = [];
-    var time = 0;
+var convertToMIDI = function(note) {
+    var dict = {a: 21, b: 23, c: 12, d: 14, e: 16, f: 17, g: 19};
+    var letter = (note.charAt(0)).toLowerCase();
+    var number = parseInt(note.charAt(note.length - 1));
+    var alteration = 0;
+    if(note.length == 3)
+        if(note.charAt(1).toLowerCase() == 'b')
+            alteration = -1;
+        if(note.charAt(1).toLowerCase() == '#')
+            alteration = 1;
+    var result = dict[letter] + number * 12 + alteration;
+
+    return result;
+}
+ 
+var compile = function (expr) {
     
-    var create_list = function(musexpr) {
-        if(musexpr.tag == 'note') {
-            musexpr.start = time;
-            time += musexpr.dur;
-            list.push(musexpr);
-        }
-        else {
-            create_list(musexpr.left);
-            create_list(musexpr.right);
-        }
+    var list = [];
+    
+    var compileT = function (musexpr, time) {
+        switch(musexpr.tag) {
+            case 'rest': {
+                var notexpr = { tag: 'rest', start: time, dur: musexpr.dur};
+                list.push(notexpr);
+                return time + notexpr.dur;
+            }
+            break;
+
+            case 'note': {
+                var notexpr = { tag: 'note', pitch: convertToMIDI(musexpr.pitch), start: time, dur: musexpr.dur};
+                list.push(notexpr);
+                return time + notexpr.dur;
+            }
+            break;
+
+            case 'seq': {
+                return compileT(musexpr.right, compileT(musexpr.left, time));
+            }
+            break;
+
+            case 'par': {
+                return Math.max(compileT(musexpr.left, time), compileT(musexpr.right, time));
+            }
+            break;
+
+            case 'repeat': {
+                var i;
+                var t = time;
+                for(i = 1; i <= musexpr.count; ++i) {
+                    t = compileT(musexpr.section, t);
+
+                }
+                return t;
+            }
+        }     
     };
-        
-    create_list(musexpr);
+    
+    compileT(expr,0);
+    
     return list;
 };
 
@@ -46,27 +88,37 @@ var playMUS = function(musexpr) {
     playNOTE(compile(musexpr));
 };
 
-var compile = function (musexpr) {
-    
-    var list = [];
-    
-    var compileT = function (musexpr, time) {
-        if(musexpr.tag == 'note') {
-            musexpr.start = time;
-            list.push(musexpr);
-            return time + musexpr.dur;
-        }
-        else if(musexpr.tag == 'seq') {
-        
-            return compileT(musexpr.right, compileT(musexpr.left, time));
-        
-            } else 
-                if (musexpr.tag == 'par') {
-                    return Math.max(compileT(musexpr.left, time), compileT(musexpr.right, time));
-                }   
-    };
-    
-    compileT(musexpr,0);
-    
-    return list;
-};
+var m1 =
+    { tag: 'seq',
+      left: 
+       { tag: 'par',
+         left: { tag: 'note', pitch: 'a#4', dur: 250 },
+         right: { tag: 'note', pitch: 'c4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'rest', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+var m2 = { tag: 'repeat',
+              section: { tag: 'par',
+                         left: { tag: 'note', pitch: 'a#4', dur: 250 },
+                         right: { tag: 'note', pitch: 'c4', dur: 250 } },
+              count: 3 };
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'par',
+         left: { tag: 'note', pitch: 'a#4', dur: 250 },
+         right: 
+            { tag: 'repeat',
+              section: { tag: 'note', pitch: 'c4', dur: 250 },
+              count: 3 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'rest', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(m2);
+console.log("\nCompiled:");
+console.log(compile(m2));
